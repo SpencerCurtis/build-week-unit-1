454df65a0d8a51c09c0f619fa09e06fe3638cad5
Spencer Curtis <spencercolecurtis@gmail.com>
2020-05-26 09:41:31 -0600

    Add new Xcode project

---
+++ BingeWatcher/BingeWatcher/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BingeWatcher
+//
+//  Created by Spencer Curtis on 5/26/20.
+//  Copyright © 2020 Spencer Curtis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
